be1e6a45c22bdbafe4ec595349432836233f38a3
888 <efremov.vrn@gmail.com>
2022-04-24 20:11:45 +0300

    042 Отображение объектов в Админке

--- personal_potfolio/urls.py
+++ personal_potfolio/urls.py
@@ -17,10 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from portfolio import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='Home')
+
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
